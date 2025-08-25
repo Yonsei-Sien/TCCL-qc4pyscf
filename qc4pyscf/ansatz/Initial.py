8c69bdd0e4540d633d08ffd42365693cab14c035
GM Kim <144093191+Yonsei-Sien@users.noreply.github.com>
2025-08-25 14:34:29 +0900

    Delete qc4pyscf directory

--- qc4pyscf/ansatz/Initial.py
+++
@@ -1,9 +0,0 @@
-from qiskit.circuit import QuantumCircuit
-
-def initial(nspace_orb, nalpha, nbeta):
-    circuit = QuantumCircuit(2*nspace_orb)
-    for i in range(nalpha):
-        circuit.x(i)
-    for j in range(nbeta):
-        circuit.x(j + nspace_orb)
-    return circuit
